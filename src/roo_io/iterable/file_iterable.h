0423e3f3dc59e07ef3ba0478d151711c8614ed31
dejwk <dawid.kurzyniec@gmail.com>
2024-11-10 01:42:30 +0100

    Added FileIterable.

---
+++ src/roo_io/iterable/file_iterable.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "roo_io/fs/filesystem.h"
+#include "roo_io/iterator/buffered_input_stream_iterator.h"
+
+namespace roo_io {
+
+class FileIterable {
+ public:
+  class FileIterator {
+   public:
+    FileIterator(Filesystem& fs, const char* path)
+        : mount_(fs.mount()), input_(mount_.fopen(path)), itr_(*input_) {}
+
+    uint8_t read() { return itr_.read(); }
+
+    int read(uint8_t* buf, unsigned int count) { return itr_.read(buf, count); }
+
+    void skip(unsigned int count) { itr_.skip(count); }
+
+    Status status() const { return itr_.status(); }
+
+   private:
+    Mount mount_;
+    std::unique_ptr<RandomAccessInputStream> input_;
+    BufferedInputStreamIterator itr_;
+  };
+
+  FileIterable(Filesystem& fs, const char* path)
+      : fs_(fs), path_(strdup(path)) {}
+
+  FileIterator iterator() const { return FileIterator(fs_, path_.get()); }
+
+ private:
+  Filesystem& fs_;
+  std::unique_ptr<char[]> path_;
+};
+
+}  // namespace roo_io
