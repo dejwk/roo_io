e34826647b25b2d70b7d5c2c96359dd343d8b8a2
dejwk <dawid.kurzyniec@gmail.com>
2024-11-26 20:00:26 +0100

    Documented the output iterator.

--- src/roo_io/iterator/output_iterator.h
+++ src/roo_io/iterator/output_iterator.h
@@ -4,17 +4,35 @@
 
 #include "roo_io/status.h"
 
-// The output iterator template contract abstracts away various sinks of
-// data, such as: DRAM, SPIFFS, or SDFS. A compliant class looks like
-// follows:
+// Output iterators define a template contract for writing streams of bytes, to
+// be used for performance-critical applications where it is desirable that byte
+// writes are inlined. An example application is to write byte-by-byte to a
+// buffered file stream.
 
 // class MyOutputIterator {
 //  public:
-//   // Writes a single byte.
+//   // Writes a single byte. Updates `status()`.
+//   //
+//   // If the status is not `kOk` before the call, the call has no effect.
+//   //
 //   void write(uint8_t v);
 //
-//   // Returns kOk if the last read or seek operation succeeded. Returns a
-//   // different status, as appropriate, if it failed, e.g. because of
-//   // encountering EOS or a read error.
+//   // Writes up to `count` bytes from the `buffer`. Updates `status()`.
+//   // Returns the number of bytes written, which must be greater than zero on
+//   // success (i.e. when `status()` returns `kOk`), and possibly zero on
+//   // error.
+//   //
+//   // If the status is not `kOk` before the call, the call has no effect and
+//   // returns zero.
+//   //
+//   // In case of error, updates the status, and returns the number of bytes
+//   // that have been written before the error was encountered (possibly zero,
+//   // but might be greater than zero).
+//   //
+//   unsigned int write(const uint8_t* buf, unsigned int count);
+//
+//   // Returns the status of the underlying stream. Updated by write
+//   // operations. Always 'kOk' or failure (never 'kEndOfStream').
+//   //
 //   Status status() const;
 // };
