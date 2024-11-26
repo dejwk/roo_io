02183d679aaf39281332c1ea2bf31004ea08487a
dejwk <dawid.kurzyniec@gmail.com>
2024-11-26 20:01:35 +0100

    Refactored byte_order to depend on platform-independent endianness.

--- src/roo_io/data/byte_order.h
+++ src/roo_io/data/byte_order.h
@@ -4,20 +4,35 @@
 // platform-independent representations that can be used as template
 // parameters.
 
-// The 'integer' template parameters are meant to take values defined in
-// <endian.h>, e.g. BYTE_ORDER, LITTLE_ENDIAN, BIG_ENDIAN.
-//
 // Note: a decent compiler will completely remove 'no-op' conversions,
 // and inline swaps using native builtins.
 
-#include <endian.h>
 #include <inttypes.h>
+#include "roo_io/third_party/endianness.h"
 
 namespace roo_io {
 
+enum ByteOrder {
+  kLittleEndian = 0,
+  kBigEndian = 1,
+
+#if defined(__BIG_ENDIAN__)
+  kNativeEndian = kBigEndian,
+#elif defined(__LITTLE_ENDIAN__)
+  kNativeEndian = kLittleEndian,
+#endif
+};
+
+}  // namespace roo_io
+
+// Determine the native byte order if possible.
+
+
+
+namespace roo_io {
 namespace byte_order {
 
-// Operator for swapping the byte order.
+// Templated operator for swapping the byte order of any unsigned integer type.
 template <typename type>
 struct Swapper;
 
@@ -29,35 +44,40 @@ struct Swapper<uint8_t> {
 template <>
 struct Swapper<uint16_t> {
   constexpr uint16_t operator()(uint16_t in) const {
-    return BYTE_ORDER == LITTLE_ENDIAN ? htobe16(in) : htole16(in);
-  }
-};
-
-template <>
-struct Swapper<uint64_t> {
-  constexpr uint64_t operator()(uint64_t in) const {
-    return BYTE_ORDER == LITTLE_ENDIAN ? htobe64(in) : htole64(in);
+    return bswap16(in);
   }
 };
 
 template <>
 struct Swapper<uint32_t> {
   constexpr uint32_t operator()(uint32_t in) const {
-    return BYTE_ORDER == LITTLE_ENDIAN ? htobe32(in) : htole32(in);
+    return bswap32(in);
   }
 };
 
+template <>
+struct Swapper<uint64_t> {
+  constexpr uint64_t operator()(uint64_t in) const {
+    return bswap64(in);
+  }
+};
+
+template <typename storage_type>
+constexpr storage_type Swap(storage_type in) {
+  return Swapper<storage_type>()(in);
+}
+
 // Operator for converting the byte order from src to dst. The default
 // implementation, that assumes that src != dst, and thus swapping is needed.
-template <typename storage_type, int byte_order_src, int byte_order_dst>
+template <typename storage_type, ByteOrder src, ByteOrder dst>
 struct Converter {
   constexpr storage_type operator()(storage_type in) const {
-    return Swapper<storage_type>()(in);
+    return Swap(in);
   }
 };
 
 // No swapping if src == dst.
-template <typename storage_type, int byte_order>
+template <typename storage_type, ByteOrder byte_order>
 struct Converter<storage_type, byte_order, byte_order> {
   constexpr storage_type operator()(storage_type in) const { return in; }
 };
@@ -65,9 +85,9 @@ struct Converter<storage_type, byte_order, byte_order> {
 // Utility to convert an integer type from the src to dst byte order.
 // If src == dst, compiles to no-op; otherwise, it compiles to
 // a built-in byte swap routine.
-template <typename storage_type, int byte_order_src, int byte_order_dst>
+template <typename storage_type, ByteOrder src, ByteOrder dst>
 constexpr storage_type Convert(storage_type in) {
-  return Converter<storage_type, byte_order_src, byte_order_dst>()(in);
+  return Converter<storage_type, src, dst>()(in);
 }
 
 }  // namespace byte_order
@@ -75,49 +95,49 @@ constexpr storage_type Convert(storage_type in) {
 // A convenience function that swaps the bytes in the integer type.
 template <typename storage_type>
 constexpr storage_type bswap(storage_type in) {
-  return byte_order::Swapper<storage_type>()(in);
+  return byte_order::Swap(in);
 }
 
 // A convenience function to convert an integer type from
 // host-native byte order to the specified byte order.
-template <typename storage_type, int byte_order_dst>
+template <typename storage_type, ByteOrder dst>
 constexpr storage_type hto(storage_type in) {
-  return byte_order::Convert<storage_type, BYTE_ORDER, byte_order_dst>(in);
+  return byte_order::Convert<storage_type, kNativeEndian, dst>(in);
 }
 
 // A convenience function to convert an integer type from
 // a specified byte order to the host-native byte order.
-template <typename storage_type, int byte_order_src>
+template <typename storage_type, ByteOrder src>
 constexpr storage_type toh(storage_type in) {
-  return byte_order::Convert<storage_type, byte_order_src, BYTE_ORDER>(in);
+  return byte_order::Convert<storage_type, src, kNativeEndian>(in);
 }
 
 // A convenience function to convert an integer type from
 // host-native byte order to big endian.
 template <typename storage_type>
 constexpr storage_type htobe(storage_type in) {
-  return byte_order::Convert<storage_type, BYTE_ORDER, BIG_ENDIAN>(in);
+  return byte_order::Convert<storage_type, kNativeEndian, kBigEndian>(in);
 }
 
 // A convenience function to convert an integer type from
 // host-native byte order to little endian.
 template <typename storage_type>
 constexpr storage_type htole(storage_type in) {
-  return byte_order::Convert<storage_type, BYTE_ORDER, LITTLE_ENDIAN>(in);
+  return byte_order::Convert<storage_type, kNativeEndian, kLittleEndian>(in);
 }
 
 // A convenience function to convert an integer type from
 // big endian to the host-native byte order.
 template <typename storage_type>
 constexpr storage_type betoh(storage_type in) {
-  return byte_order::Convert<storage_type, BIG_ENDIAN, BYTE_ORDER>(in);
+  return byte_order::Convert<storage_type, kBigEndian, kNativeEndian>(in);
 }
 
 // A convenience function to convert an integer type from
 // little endian to the host-native byte order.
 template <typename storage_type>
 constexpr storage_type letoh(storage_type in) {
-  return byte_order::Convert<storage_type, LITTLE_ENDIAN, BYTE_ORDER>(in);
+  return byte_order::Convert<storage_type, kLittleEndian, kNativeEndian>(in);
 }
 
 }  // namespace roo_io
--- test/data/byte_order_test.cpp
+++ test/data/byte_order_test.cpp
@@ -10,17 +10,17 @@ static const uint32_t i32 = 0x11223344;
 using byte_order::Convert;
 
 TEST(ByteOrder, Converts16) {
-  EXPECT_EQ(0x1122, (Convert<uint16_t, BIG_ENDIAN, BIG_ENDIAN>(i16)));
-  EXPECT_EQ(0x1122, (Convert<uint16_t, LITTLE_ENDIAN, LITTLE_ENDIAN>(i16)));
-  EXPECT_EQ(0x2211, (Convert<uint16_t, BIG_ENDIAN, LITTLE_ENDIAN>(i16)));
-  EXPECT_EQ(0x2211, (Convert<uint16_t, LITTLE_ENDIAN, BIG_ENDIAN>(i16)));
+  EXPECT_EQ(0x1122, (Convert<uint16_t, kBigEndian, kBigEndian>(i16)));
+  EXPECT_EQ(0x1122, (Convert<uint16_t, kLittleEndian, kLittleEndian>(i16)));
+  EXPECT_EQ(0x2211, (Convert<uint16_t, kBigEndian, kLittleEndian>(i16)));
+  EXPECT_EQ(0x2211, (Convert<uint16_t, kLittleEndian, kBigEndian>(i16)));
 }
 
 TEST(ByteOrder, Converts32) {
-  EXPECT_EQ(0x11223344, (Convert<uint32_t, BIG_ENDIAN, BIG_ENDIAN>(i32)));
-  EXPECT_EQ(0x11223344, (Convert<uint32_t, LITTLE_ENDIAN, LITTLE_ENDIAN>(i32)));
-  EXPECT_EQ(0x44332211, (Convert<uint32_t, BIG_ENDIAN, LITTLE_ENDIAN>(i32)));
-  EXPECT_EQ(0x44332211, (Convert<uint32_t, LITTLE_ENDIAN, BIG_ENDIAN>(i32)));
+  EXPECT_EQ(0x11223344, (Convert<uint32_t, kBigEndian, kBigEndian>(i32)));
+  EXPECT_EQ(0x11223344, (Convert<uint32_t, kLittleEndian, kLittleEndian>(i32)));
+  EXPECT_EQ(0x44332211, (Convert<uint32_t, kBigEndian, kLittleEndian>(i32)));
+  EXPECT_EQ(0x44332211, (Convert<uint32_t, kLittleEndian, kBigEndian>(i32)));
 }
 
 }  // namespace roo_io
