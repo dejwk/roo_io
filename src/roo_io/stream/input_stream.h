b25ab4c7e9fcb032c5a01d56952bf95d57522542
dejwk <dawid.kurzyniec@gmail.com>
2024-12-05 21:37:31 +0100

    Clarify the contract of input streams.

--- src/roo_io/stream/input_stream.h
+++ src/roo_io/stream/input_stream.h
@@ -22,6 +22,17 @@ class InputStream {
   // (i.e. when `status()` returns 'kOk'), zero on end-of-stream (i.e. when
   // `status()` returns 'kEndOfStream'), and possibly zero on error (i.e.
   // when `status()` returns another value).
+  //
+  // If the status was not 'kOk' prior to the call, leaves it as is and returns
+  // zero. Otherwise, if there are less then `count` but more than zero bytes
+  // available before the end of stream, reads and returns the available bytes,
+  // and leaves status as `kOk`. Otherwise if there are no more bytes available
+  // before end of stream, returns zero and updates status to `kEndOfStream`.
+  //
+  // In case of error, updates the status, and returns the number of bytes that
+  // have been read before the error was encountered (possibly zero, but might
+  // be greater than zero).
+  //
   virtual size_t read(byte* result, size_t count) = 0;
 
   // Reads the prescribed number of bytes, blocking if necessary.
@@ -37,6 +48,15 @@ class InputStream {
     return read_total;
   }
 
+  // Skips over the specified count of bytes. Updates the `status()`.
+  //
+  // If the status was not `kOk` prior to the call, leaves it as is. Otherwise,
+  // if the skip was succesful and the stream is before or exactly at the end,
+  // leaves the status as `kOk`. Otherwise, if the skip moves the stream past
+  // the end, updates the status to `kEndOfStream`.
+  //
+  // In case of error, updates the status accordingly.
+  //
   virtual void skip(uint64_t count) = 0;
 
   // Returns the status of the most recent I/O operation.
--- src/roo_io/stream/multipass_input_stream.h
+++ src/roo_io/stream/multipass_input_stream.h
@@ -10,7 +10,15 @@ namespace roo_io {
 // resource with a 'file pointer'.
 class MultipassInputStream : public InputStream {
  public:
-  // Returns the size of the resource in bytes. The stream must be open.
+  // Returns the total number of bytes comprising the stream, counting from the
+  // beginning. The method can return different value each time it is called,
+  // e.g. when the underlying data source is concurrently written to.
+  //
+  // If the status was not `kOk` and not `kEndOfStream` at the time of the call,
+  // leaves it as is and returns zero.
+  //
+  // In case of error, updates the status accordingly, and returns zero.
+  //
   virtual uint64_t size() = 0;
 
   // Returns the current position in the stream, in bytes. The stream must be
@@ -19,9 +27,6 @@ class MultipassInputStream : public InputStream {
 
   // Returns true on success.
   virtual void seek(uint64_t offset) = 0;
-
-  // Returns true on success.
-  virtual void skip(uint64_t count) = 0;
 };
 
 }  // namespace roo_io
