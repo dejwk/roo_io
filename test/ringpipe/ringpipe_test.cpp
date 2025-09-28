3c470de85fe36d770132b6852491ea5c000ea28a
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-09-28 20:34:18 +0200

    Added ringpipe tests for input/output streams.

--- test/ringpipe/ringpipe_test.cpp
+++ test/ringpipe/ringpipe_test.cpp
@@ -1,12 +1,13 @@
 #include "roo_io/ringpipe/ringpipe.h"
 
-#include "roo_threads/thread.h"
-
 #include "gtest/gtest.h"
+#include "roo_io/ringpipe/ringpipe_input_stream.h"
+#include "roo_io/ringpipe/ringpipe_output_stream.h"
+#include "roo_threads/thread.h"
 
 namespace roo_io {
 
-TEST(PipeTest, BasicWriteRead) {
+TEST(RingPipe, BasicWriteRead) {
   RingPipe pipe(4);
   byte data[] = {byte{1}, byte{2}, byte{3}};
   size_t written = pipe.write(data, 3);
@@ -24,7 +25,7 @@ TEST(PipeTest, BasicWriteRead) {
   EXPECT_EQ(pipe.availableForWrite(), 4);
 }
 
-TEST(PipeTest, WriteMoreThanCapacity) {
+TEST(RingPipe, WriteMoreThanCapacity) {
   RingPipe pipe(2);
   byte data[] = {byte{1}, byte{2}, byte{3}};
   size_t written = pipe.write(data, 3);
@@ -33,7 +34,7 @@ TEST(PipeTest, WriteMoreThanCapacity) {
   EXPECT_EQ(pipe.availableForWrite(), 0);
 }
 
-TEST(PipeTest, CloseUnblocksRead) {
+TEST(RingPipe, CloseUnblocksRead) {
   RingPipe pipe(2);
   byte data[] = {byte{1}, byte{2}};
   size_t written = pipe.write(data, 2);
@@ -54,7 +55,7 @@ TEST(PipeTest, CloseUnblocksRead) {
   EXPECT_EQ(0, pipe.availableForWrite());
 }
 
-TEST(PipeTest, CloseAwakesReadAsync) {
+TEST(RingPipe, CloseAwakesReadAsync) {
   RingPipe pipe(2);
   byte out[2];
 
@@ -73,7 +74,7 @@ TEST(PipeTest, CloseAwakesReadAsync) {
   reader.join();
 }
 
-TEST(PipeTest, ClosingInputPreventsFurtherWrites) {
+TEST(RingPipe, ClosingInputPreventsFurtherWrites) {
   RingPipe pipe(2);
   pipe.closeInput();
   EXPECT_EQ(pipe.inputStatus(), kClosed);
@@ -84,6 +85,79 @@ TEST(PipeTest, ClosingInputPreventsFurtherWrites) {
   EXPECT_EQ(pipe.outputStatus(), kBrokenPipe);
   EXPECT_EQ(0, pipe.availableForRead());
   EXPECT_EQ(0, pipe.availableForWrite());
+}
+
+TEST(RingPipe, BasicStreams) {
+  RingPipe pipe(128);
+  RingPipeOutputStream output_stream(pipe);
+  RingPipeInputStream input_stream(pipe);
+  EXPECT_TRUE(output_stream.isOpen());
+  EXPECT_TRUE(input_stream.isOpen());
+  EXPECT_EQ(output_stream.status(), kOk);
+  EXPECT_EQ(input_stream.status(), kOk);
+  EXPECT_EQ(output_stream.availableForWrite(), 128);
+  EXPECT_EQ(input_stream.available(), 0);
+  byte data[] = {byte{1}, byte{2}, byte{3}};
+  size_t written = output_stream.write(data, 3);
+  EXPECT_EQ(written, 3);
+  EXPECT_EQ(output_stream.availableForWrite(), 125);
+  EXPECT_EQ(input_stream.available(), 3);
+  byte out[3];
+  size_t read = input_stream.read(out, 3);
+  EXPECT_EQ(read, 3);
+  EXPECT_EQ(out[0], byte{1});
+  EXPECT_EQ(out[1], byte{2});
+  EXPECT_EQ(out[2], byte{3});
+  EXPECT_EQ(output_stream.availableForWrite(), 128);
+  EXPECT_EQ(input_stream.available(), 0);
+  output_stream.close();
+  EXPECT_FALSE(output_stream.isOpen());
+  EXPECT_EQ(output_stream.status(), kClosed);
+  EXPECT_TRUE(input_stream.isOpen());
+  EXPECT_EQ(input_stream.status(), kEndOfStream);
+  input_stream.close();
+  EXPECT_FALSE(input_stream.isOpen());
+  EXPECT_EQ(input_stream.status(), kClosed);
+}
+
+TEST(RingPipe, StreamsDataTransfer) {
+  RingPipe pipe(128);
+  RingPipeOutputStream output_stream(pipe);
+  RingPipeInputStream input_stream(pipe);
+  const size_t data_size = 1000000;
+  byte* data = new byte[data_size];
+  for (size_t i = 0; i < data_size; ++i) {
+    data[i] = (byte)(i & 0xFF);
+  }
+  roo::thread writer([&output_stream, data, data_size]() {
+    size_t total_written = 0;
+    while (total_written < data_size) {
+      size_t written =
+          output_stream.write(data + total_written, data_size - total_written);
+      if (written > 0) {
+        total_written += written;
+      }
+      roo::this_thread::yield();
+    }
+    output_stream.close();
+  });
+  byte* in = new byte[data_size];
+  size_t total_read = 0;
+  while (total_read < data_size) {
+    size_t read = input_stream.read(in + total_read, data_size - total_read);
+    if (read == 0) break;
+    total_read += read;
+    roo::this_thread::yield();
+  }
+  EXPECT_EQ(input_stream.status(), kEndOfStream);
+  input_stream.close();
+  writer.join();
+  EXPECT_EQ(total_read, data_size);
+  for (size_t i = 0; i < data_size; ++i) {
+    EXPECT_EQ(in[i], byte{i & 0xFF});
+  }
+  delete[] data;
+  delete[] in;
 }
 
 }  // namespace roo_io
