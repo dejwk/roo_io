3167533ef9c47f9c825d10f1d0396317d3f2c7c4
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-10-14 02:00:30 +0200

    Adding features to the Arduino SD: read-only mounts, and on ESP32, also the option to set max open files, mount point, and whether to format if mount failed.

--- src/roo_io/fs/arduino/sdfs.cpp
+++ src/roo_io/fs/arduino/sdfs.cpp
@@ -1,9 +1,9 @@
 
 #ifdef ARDUINO
 
-#include <Arduino.h>
-
 #include "roo_io/fs/arduino/sdfs.h"
+
+#include <Arduino.h>
 
 #ifdef ESP32
 // Directly use the lower-level POSIX APIs, bypassing Arduino filesystem stuff
@@ -21,15 +21,43 @@ namespace roo_io {
 ArduinoSdFs::ArduinoSdFs(uint8_t cs_pin, decltype(::SD)& sd,
                          decltype(::SPI)& spi, uint32_t freq)
 
-    : cs_pin_(cs_pin), sd_(sd), spi_(&spi), frequency_(freq) {}
+    : cs_pin_(cs_pin),
+      sd_(sd),
+      spi_(&spi),
+      frequency_(freq),
+      mount_point_("/sd"),
+      max_open_files_(5),
+      format_if_mount_failed_(false),
+      read_only_(false) {}
+
+const char* ArduinoSdFs::mountPoint() const { return mount_point_.c_str(); }
+
+void ArduinoSdFs::setMountPoint(const char* mount_point) {
+  mount_point_ = mount_point;
+}
+
+uint8_t ArduinoSdFs::maxOpenFiles() const { return max_open_files_; }
+
+void ArduinoSdFs::setMaxOpenFiles(uint8_t max_open_files) {
+  max_open_files_ = max_open_files;
+}
+
+bool ArduinoSdFs::formatIfMountFailed() const {
+  return format_if_mount_failed_;
+}
+
+void ArduinoSdFs::setFormatIfMountFailed(bool format_if_mount_failed) {
+  format_if_mount_failed_ = format_if_mount_failed;
+}
 
 MountImpl::MountResult ArduinoSdFs::mountImpl(
     std::function<void()> unmount_fn) {
-  if (!sd_.begin(cs_pin_, *spi_, frequency_)) {
+  if (!sd_.begin(cs_pin_, *spi_, frequency_, mount_point_.c_str(),
+                 max_open_files_, format_if_mount_failed_)) {
     return MountImpl::MountError(kGenericMountError);
   }
   return MountImpl::Mounted(std::unique_ptr<MountImpl>(
-      new PosixMountImpl(sd_.mountpoint(), false, unmount_fn)));
+      new PosixMountImpl(sd_.mountpoint(), read_only_, unmount_fn)));
 }
 
 Filesystem::MediaPresence ArduinoSdFs::checkMediaPresence() {
@@ -38,15 +66,16 @@ Filesystem::MediaPresence ArduinoSdFs::checkMediaPresence() {
 
 #else
 
-ArduinoSdFs::ArduinoSdFs(uint8_t cs_pin) : sd_(::SD), cs_pin_(cs_pin) {}
+ArduinoSdFs::ArduinoSdFs(uint8_t cs_pin)
+    : sd_(::SD), cs_pin_(cs_pin), read_only_(false) {}
 
 MountImpl::MountResult ArduinoSdFs::mountImpl(
     std::function<void()> unmount_fn) {
   if (!sd_.begin(cs_pin_)) {
     return MountImpl::MountError(kGenericMountError);
   }
-  return MountImpl::Mounted(
-      std::unique_ptr<MountImpl>(new ArduinoMountImpl(sd_, false, unmount_fn)));
+  return MountImpl::Mounted(std::unique_ptr<MountImpl>(
+      new ArduinoMountImpl(sd_, read_only_, unmount_fn)));
 }
 
 Filesystem::MediaPresence ArduinoSdFs::checkMediaPresence() {
@@ -57,7 +86,9 @@ Filesystem::MediaPresence ArduinoSdFs::checkMediaPresence() {
 
 void ArduinoSdFs::unmountImpl() { sd_.end(); }
 
-ArduinoSdFs SD;
+ArduinoSdFs CreateArduinoSdFs() { return ArduinoSdFs(); }
+
+ArduinoSdFs SD = CreateArduinoSdFs();
 
 }  // namespace roo_io
 
--- src/roo_io/fs/arduino/sdfs.h
+++ src/roo_io/fs/arduino/sdfs.h
@@ -15,9 +15,6 @@ namespace roo_io {
 
 class ArduinoSdFs : public Filesystem {
  public:
-  ArduinoSdFs(uint8_t cs_pin = SS, decltype(::SD)& sd = ::SD,
-              decltype(::SPI)& spi = ::SPI, uint32_t freq = 20000000);
-
   MediaPresence checkMediaPresence() override;
 
   void setCsPin(uint8_t cs_pin) { cs_pin_ = cs_pin; }
@@ -25,7 +22,24 @@ class ArduinoSdFs : public Filesystem {
   void setSPI(decltype(::SPI)& spi) { spi_ = &spi; }
   void setFrequency(uint32_t freq) { frequency_ = freq; }
 
+  const char* mountPoint() const;
+  void setMountPoint(const char* mount_point);
+
+  uint8_t maxOpenFiles() const;
+  void setMaxOpenFiles(uint8_t max_open_files);
+
+  bool formatIfMountFailed() const;
+  void setFormatIfMountFailed(bool format_if_mount_failed);
+
+  bool readOnly() const { return read_only_; }
+  void setReadOnly(bool read_only) { read_only_ = read_only; }
+
  protected:
+  friend ArduinoSdFs CreateArduinoSdFs();
+
+  ArduinoSdFs(uint8_t cs_pin = SS, decltype(::SD)& sd = ::SD,
+              decltype(::SPI)& spi = ::SPI, uint32_t freq = 20000000);
+
   MountImpl::MountResult mountImpl(std::function<void()> unmount_fn) override;
 
   void unmountImpl() override;
@@ -35,25 +49,37 @@ class ArduinoSdFs : public Filesystem {
   decltype(::SD)& sd_;
   decltype(::SPI)* spi_;
   uint32_t frequency_;
+
+  std::string mount_point_;
+  uint8_t max_open_files_;
+  bool format_if_mount_failed_;
+  bool read_only_;
 };
 
 #else
 
 class ArduinoSdFs : public Filesystem {
  public:
-  ArduinoSdFs(uint8_t cs_pin = SS);
-
   MediaPresence checkMediaPresence() override;
 
   void setCsPin(uint8_t cs_pin) { cs_pin_ = cs_pin; }
 
+  bool readOnly() const { return read_only_; }
+  void setReadOnly(bool read_only) { read_only_ = read_only; }
+
  protected:
+  friend ArduinoSdFs CreateArduinoSdFs();
+
+  ArduinoSdFs(uint8_t cs_pin = SS);
+
   MountImpl::MountResult mountImpl(std::function<void()> unmount_fn) override;
 
   void unmountImpl() override;
 
   uint8_t cs_pin_;
   decltype(::SD)& sd_;
+
+  bool read_only_;
 };
 
 #endif
